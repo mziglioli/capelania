e352713156c670b2b6269b7c09f11ff1e27b8562
zigliolim <MarceloZiglioli@rentalcars.com>
2019-07-29 09:11:16 +0100

    init

---
+++ src/main/java/com/capelania/CapelaniaApplication.java
@@ -0,0 +1,13 @@
+package com.capelania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapelaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapelaniaApplication.class, args);
+	}
+
+}
